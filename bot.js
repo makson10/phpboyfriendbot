afb0c71704fa5caa7813c1c77100fa8448bb4e65
Maks <makson94021@gmail.com>
2023-11-16 21:50:40 +0200

    �

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const TelegramBot = require("node-telegram-bot-api");
 const TOKEN = process.env.TOKEN;
 
 //* For Dev:
-const bot = new TelegramBot(TOKEN, { polling: true });
+// const bot = new TelegramBot(TOKEN, { polling: true });
 
 //* For Prod:
-// const bot = new TelegramBot(TOKEN);
-// bot.setWebHook('https://phpboyfriendbotwebhook.onrender.com/' + TOKEN);
+const bot = new TelegramBot(TOKEN);
+bot.setWebHook('https://phpboyfriendbotwebhook.onrender.com/' + TOKEN);
 
 module.exports = bot;
