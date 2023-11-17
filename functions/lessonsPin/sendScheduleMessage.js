c54eaad14f779aaa3c003739aa7681c5080f68c0
Maks <makson94021@gmail.com>
2023-11-17 11:30:20 +0200

    test fix bug

--- functions/lessonsPin/sendScheduleMessage.js
+++ functions/lessonsPin/sendScheduleMessage.js
@@ -5,10 +5,6 @@ const getScheduleMessage = require("./getScheduleMessage");
 const { default: axios } = require("axios");
 const { renderLinkMessage } = require("../linkMessage");
 
-const todayDate = new Date();
-const todayHours = todayDate.getHours();
-const todayDay = todayDate.getDay();
-
 const deleteOldHw = async () => {
     await axios.post(process.env.MEDIATOR_BASE_URL + '/hw/removeOldHwLinks');
     await renderLinkMessage();
@@ -17,6 +13,10 @@ const deleteOldHw = async () => {
 const sendScheduleMessage = async (msg) => {
     const chatId = msg.chat.id;
     const messageId = msg.message_id;
+
+    const todayDate = new Date();
+    const todayHours = todayDate.getUTCHours();
+    const todayDay = todayDate.getUTCDay();
 
     const supergroupId = await getSupergroupId();
     await bot.deleteMessage(chatId, messageId);
@@ -32,7 +32,10 @@ const sendScheduleMessage = async (msg) => {
     const newScheduleMessage = await bot.sendMessage(supergroupId, scheduleMessages, { parse_mode: 'HTML' });
     await handleLessonSchedule(newScheduleMessage);
 
-    if (todayHours >= 21 || todayHours === 0 || todayHours === 1) await deleteOldHw();
+    if (todayHours === 9 || todayHours >= 20) await deleteOldHw();
 }
 
 module.exports = sendScheduleMessage;
+
+// 22 23 00 01
+// 20 21 22 23
